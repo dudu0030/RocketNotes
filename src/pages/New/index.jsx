95d0643c53acb4759937be5a93874c4d67d23f9c
Eduardo <duduepifanio123@gmail.com>
2024-01-18 13:55:04 -0300

    Config validação da nota

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -45,6 +45,18 @@ export function New() {
   }
 
   async function handleNewNote(){
+    if (!title) {
+      return alert("Digite o título da nota")
+    }
+
+    if (newLink) {
+      return alert("Você deixou um link no campo para adicionar, mas não clicou em adicionar")
+    }
+    
+    if (newTag) {
+      return alert("Você deixou uma tag no campo para adicionar, mas não clicou em adicionar")
+    }
+
     await api.post("/notes", {
       title,
       description,
