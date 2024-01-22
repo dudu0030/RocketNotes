1ff15ebdbb7bd536b6ba1d2fa0a0587c9801bcd4
Eduardo <duduepifanio123@gmail.com>
2024-01-22 13:41:36 -0300

    Config tags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,8 @@
+import { useState, useEffect } from 'react'
 import { FiPlus } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
+
+import { api } from '../../services/api'
 
 import { Note } from '../../components/Note'
 import { Input } from '../../components/Input'
@@ -8,6 +11,31 @@ import { Section } from '../../components/Section'
 import { ButtonText } from '../../components/ButtonText'
 
 export function Home() {
+  const [tags, setTags] = useState([])
+  const [tagsSelected, setTagsSelected] = useState([])
+
+  function handleTagSelected(tagName) {
+    const alreadySelected = tagsSelected.includes(tagName)
+
+    if (alreadySelected) {
+      const filteredTags = tagsSelected.filter(tag => tag !== tagName)
+      setTagsSelected(filteredTags)
+
+    } else {
+      setTagsSelected(prevState => [...prevState, tagName])
+    }
+
+  }
+
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get("/tags")
+      setTags(response.data)
+
+    }
+      fetchTags()
+  },[])
+
   return (
     <Container>
       <Brand>
@@ -17,9 +45,23 @@ export function Home() {
       <Header />
 
       <Menu>
-        <li><ButtonText title="Todos" $isactive /></li>
-        <li><ButtonText title="React" /></li>
-        <li><ButtonText title="Nodejs" /></li>
+      <li><ButtonText 
+      title="Todos"
+      onClick={() => handleTagSelected("todos")}
+      $isactive={tagsSelected.length === 0} 
+      />
+      </li>
+        {
+          tags && tags.map(tag => (
+            <li 
+            key={String(tag.id)}><ButtonText 
+            title={tag.name} 
+            onClick={() => handleTagSelected(tag.name)}
+            $isactive={tagsSelected.includes(tag.name)} 
+            />
+            </li>
+          ))
+        }
       </Menu>
 
       <Search>
