e5ed00616ea741f217356fddb667ac0e5263e720
Eduardo <duduepifanio123@gmail.com>
2024-01-24 14:05:54 -0300

    Bug fixes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom'
 import { RiShutDownLine } from 'react-icons/ri'
 import { useAuth } from '../../hooks/auth'
 import { api } from '../../services/api'
@@ -6,6 +7,12 @@ import { Container, Profile, Logout } from './styles'
 
 export function Header(){
     const { signOut, user } = useAuth()
+    const navigation = useNavigate()
+
+    function handleSignOut() {
+        navigation("/")
+        signOut()
+    }
 
   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
 
@@ -21,7 +28,7 @@ export function Header(){
                     <strong>{user.name}</strong>
                 </div>
             </Profile>
-            <Logout onClick={signOut}>
+            <Logout onClick={handlesignOut}>
                 <RiShutDownLine />
             </Logout>
         </Container>
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -22,14 +22,16 @@ export function Profile() {
   const [avatarFile, setAvatarFile] = useState(null)
 
   async function handleUpdate() {
-    const user ={
+    const updated ={
       name,
       email,
       password: passwordNew,
       old_password: passwordOld,
     }
 
-    await updateProfile({ user, avatarFile })
+    const userUpdated = Object.assign(updated, user)
+
+    await updateProfile({ user: userUpdated, avatarFile })
   }
 
   async function handleChangeAvatar(event) {
