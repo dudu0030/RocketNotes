c7380a4ce9fe910935e5547b382bd109ed752beb
Eduardo <duduepifanio123@gmail.com>
2024-01-25 14:30:13 -0300

    Integration bug fix

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import { New } from '../pages/New'
 import { Home } from '../pages/Home'
@@ -6,13 +6,12 @@ import { Profile } from '../pages/Profile'
 import { Details } from '../pages/Details'
 
 export function AppRoutes() {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/new" element={<New />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/details/:id" element={<Details />} />
+    </Routes>
+  )
 }
--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,16 +1,12 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom";
 
-import { SignIn } from '../pages/SignIn'
-import { SignUp } from '../pages/SignUp'
+import { AppRoutes } from "./app.routes";
+import { AuthRoutes } from "./auth.routes";
 
-export function AuthRoutes() {
-    const user = localStorage.getItem("@rocketnotes:user")
-
-    return (
-        <Routes>
-            <Route path="/" element={<SignIn />} />
-            <Route path="/resgister" element={<SignUp />} />
-            { !user && <Route path="*" element={<Navigate to="/" />} /> }
-        </Routes>
-    )
+export function Routes() {
+  return (
+    <BrowserRouter>
+      <AuthRoutes />
+    </BrowserRouter>
+  )
 }
